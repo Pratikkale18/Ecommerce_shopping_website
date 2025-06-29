8b07118add55a26788af378684d4730e8867576a
Pratikkale18 <pratikkale0111@gamil.com>
2025-06-29 17:44:35 +0530

    Inital commit

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ import { Provider } from "react-redux";
 import store from './redux/Store'; // Make sure this path is correct
 import { ToastContainer } from "react-toastify";
 import { Toaster } from "react-hot-toast"; 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
   <Provider store={store}> {/* Make sure store is correctly imported */}
     <App />
     <Toaster/>
   </Provider>
-  </BrowserRouter>
+  </HashRouter>
  
 );
